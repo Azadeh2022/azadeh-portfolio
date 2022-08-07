307dc0d8c16dd46eea98f58d54c2c31e9465a14f
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-08-07 17:23:04 -0400

    commit

---
+++ client/src/data.js
@@ -0,0 +1,155 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "Social Media App",
+      img: "assets/social1.png"},
+    {
+      id: 2,
+      title: "UI Design",
+      img: "assets/web.png"},
+    {
+      id: 3,
+      title: "E-commerce Web Design",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    },
+    // {
+    //   id: 4,
+    //   title: "Relax Mobile App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+    // },
+    {
+      id: 5,
+      title: "Web Design",
+      img:
+        "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    },
+    // {
+    //   id: 6,
+    //   title: "Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "Web Social Media App",
+      img: "assets/social.png" },
+    {
+      id: 2,
+      title: "Web UI Design",
+      img: "assets/ui.png",  },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "assets/ecommerce.png",  },
+    {
+    //   id: 4,
+    //   title: "Web Relax App",
+    //   img: "assets/mary.jpeg", },
+    // {
+      id: 5,
+      title: "Web Design",
+      img: "assets/web1.png",
+    },
+    // {
+    //   id: 6,
+    //   title: "Web Banking App",
+    //   img: "assets/mary.jpeg",},
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Mobile Social Media App",
+      img: "assets/social.png" },
+    {
+      id: 2,
+      title: "Mobile Rampa UI Design",
+      img: "assets/Hedayati.jpeg",
+        },
+    {
+      id: 3,
+      title: "Mobile E-commerce Design",
+      img:
+        "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 4,
+      title: "Mobile Relax App",
+      img:
+        "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 5,
+      title: "Mobile Hero Design",
+      img:
+        "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+    },
+    {
+      id: 6,
+      title: "Mobile Banking App",
+      img:
+        "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "Design Personal web site. 2014",
+      img: "assets/web.png", },
+    {
+      id: 2,
+      title: "Design Yekom Intranet. 2010-2014",
+      img: "assets/web1.png",},
+    {
+      id: 3,
+      title: " SARI ZOO Web Site , 2011",
+      img: "assets/web2.png", },
+    {
+      id: 4,
+      title: "Design Yekom Web site, 2006",
+      img: "assets/web3.png", },
+    {
+      id: 5,
+      title: "Research Institute of Cultural Heritage & Tourism. 2008",
+      img: "assets/web4.png", },
+    {
+      id: 6,
+      title: "MAREMAT VA PAJOHEPSH Journal Web site, 2007",
+      img: "assets/web5.png",},
+     { id: 7,
+      title: "Joyab No Company Intranet, 2009",
+      img: "assets/web6.png",},
+      { id: 7,
+        title: "Design Book Web Site, 2006 ",
+        img: "assets/web7.png",},
+        {
+          id: 8,
+          title: "Design Yekom web, 2010",
+          img: "assets/web.jpg", },
+        
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "Chiristmas at school",
+      img: "assets/con1.png",
+     },
+    {
+      id: 2,
+      title: "YouTube Channel",
+      img: "assets/con2.png",
+     },
+    {
+      id: 3,
+      title: "YouTube Channel",
+      img: "assets/con3.png",
+    },
+    
+  ];
